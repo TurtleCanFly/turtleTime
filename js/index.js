240b074642a4c703c0adc908d3c1e24f08f9d604
xSeanliux <zhxnliu@gmail.com>
2018-07-11 00:04:38 +0800

    Added Animation
    turtle animation

--- js/index.js
+++ js/index.js
@@ -19,6 +19,35 @@ $("#btn-left").click(function(){
 function run(){
 	res = todos.find();
 	for(var i = 0 ; i < res.length; i++){
-		$("#toDoList").append('<p class="list-group-item">' + res[i].name + '</p>');
+		$("#toDoList").append('<p class="list-group-item">' + res[i].name + '<input type="checkbox" class="checkbox" id = item_' + i + '></p>');
 	}
 }
+
+function changePer(n){
+	$("#turtle").css("clip", "rect(" + 280*(1 - (n)/100) +", 454px, 284px, 0)"); //PLEASE DO NOT CHANGE THE NUMBERS IT TOOK HALF AN HOUR TO GET THE RIGHT NUMBERS
+	//console.log(n);
+}
+
+
+$(document).on('change', '.checkbox', function() {
+    if(this.checked) {
+        // checkbox is checked
+        var id = this.id;
+        id = parseInt(id.substring(5));
+        res = todos.find()[id];
+        todos.updateById(id, {isDone, !(res.isDone)});
+	    console.log(!res.isDone);
+    }
+});
+
+for(var i = 0 ; i < 10; i++){
+	todos.insert({
+	subject: "math",
+	name: "AIME",
+	date: new Date(),
+	isDone: false
+	});
+}
+
+run();
+
