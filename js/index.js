c741dd31447c06351477cd71d2d10d20e54169b7
xSeanliux <zhxnliu@gmail.com>
2018-07-10 20:33:39 +0800

    Added FDB
    added a link to forerunners to the site, and added test material
    (incomplete)

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var ForerunnerDB = require("forerunnerdb");
+var fdb = new ForerunnerDB();
+
+var db = fdb.db("todoDB");
+var todos = db.collection("todos", {autoCreate: true}); //make the actual database
+
+/*
+Data structure:
+	Name
+	Date (): new Date(2018, 11, 24, 10, 33); //year, month, day, hour, min
+	isDone (if true then remove this item
+*/
+
+todos.insert(
+	{
+		name: "蕭梓宏",
+		date: new Date(2018, 11, 24)
+	}
+);
+
+todos.insert(
+	{
+		name: "王政祺",
+		date: new Date(2018, 11, 24)
+	}
+);
+
+
+res = todos.find({
+	name: $exists
+});
+
+for(var i = 0 ; i < res.length(); i++){
+	$(".list-group").append("")
+}
