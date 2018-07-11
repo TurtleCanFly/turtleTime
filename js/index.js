c66c7e1f14f1e092190ae574fd266de4770af071
casperwang <casperwang2014@gmail.com>
2018-07-11 13:10:29 +0800

    submit 12423

--- js/index.js
+++ js/index.js
@@ -140,5 +140,4 @@ window.onload = function () {
     
     
 }
-run();
 
--- js/view_schedule.js
+++ js/view_schedule.js
@@ -1,15 +1,17 @@
 var d = 0;
 function run(){
     s = schedule.find();
+    $("#dataList").append('<li class="date">今天</li>');
     for(var i = 0 ; i < s.length; i++) {
         //console.log(res[i]);
         var date = new Date(s[i].date);
         if(new Date().setHours(0,0,0,0)+(d*86400000) == date.setHours(0,0,0,0)) {
         // Date equals today's date  
-            $("#dataList").append('<tr><td style="font-size:50px;">' + s[i].name + '</td></tr>');
+            $("#dataList").append('<tr><td style="font-size:40px;">' + s[i].name + '</td></tr>');
         } else {
             d++;
-            $("#dataList").append('<li class="date">' + d + '</li>' + '<tr><td style="font-size:50px;">' + s[i].name + '</td></tr>');
+            $("#dataList").append('<li class="date">' + d + '天後</li>');
+            $("#dataList").append('<tr><td style="font-size:40px;">' + s[i].name + '</td></tr>');
         }
     }
 }
